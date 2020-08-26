56e7069c2cd900c9ec63ada8b0059f62a428276b
Murad-code <murad.coding@gmail.com>
2020-08-26 14:14:40 +0100

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 import Home from './components/Home/Home.jsx';
-import Navbar from './components/Navbar/Navbar.js';
+import Navbar from './components/Navbar/Navbar.jsx';
 import AboutMe from './components/AboutMe/AboutMe.jsx';
 import MySkills from './components/MySkills/MySkills.jsx';
 import Projects from './components/Projects/Projects.jsx';
@@ -9,8 +9,8 @@ import Projects from './components/Projects/Projects.jsx';
 function App() {
   return (
     <div id="App" className="App" data-aos="fade-down" >
-      <Navbar />
       <Home />
+      <Navbar />
       <AboutMe />
       <MySkills />
       <Projects />
